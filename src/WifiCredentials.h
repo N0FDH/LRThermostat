4c9fdf06dc78a8c1e30eb646358d548e0742ba33
Randy Rysavy <randy.rysavy@gmail.com>
2022-02-17 11:38:21 -0600

    create template file for credentials

--- src/WifiCredentials.h
+++
@@ -1,2 +0,0 @@
-const char *ssid = "yourSSID";         // WiFi SSID     replace with details for your local network
-const char *password = "yourPASSWORD"; // WiFi Password replace with details for your local network
